cbd608b2a484953f0e323fc5e9c5e454a0b7f6eb
Miriam de Almeida <32798904+MiriamAlmeida@users.noreply.github.com>
2022-11-15 16:48:09 -0300

    Add files via upload

--- sparkcode/titanic_example_delta.py
+++ sparkcode/titanic_example_delta.py
@@ -17,12 +17,12 @@ print("Reading CSV file from S3...")
 
 schema = "PassengerId int, Survived int, Pclass int, Name string, Sex string, Age double, SibSp int, Parch int, Ticket string, Fare double, Cabin string, Embarked string"
 df = spark.read.csv(
-    "s3://dl-landing-zone-539445819060/titanic", 
+    "s3://miriam-341567884088/titanic", 
     header=True, schema=schema, sep=";"
 )
 
 print("Writing titanic dataset as a delta table...")
-df.write.format("delta").mode("overwrite").save("s3://dl-processing-zone-539445819060/silver/titanic_delta")
+df.write.format("delta").mode("overwrite").save("s3://dl-processing-zone-341567884088/silver/titanic_delta")
 
 print("Updating and inserting new rows...")
 new = df.where("PassengerId IN (1, 5)")
@@ -35,7 +35,7 @@ newrowsdf = spark.createDataFrame(newrows, schema=schema)
 new = new.union(newrowsdf)
 
 print("Create a delta table object...")
-old = DeltaTable.forPath(spark, "s3://dl-processing-zone-539445819060/silver/titanic_delta")
+old = DeltaTable.forPath(spark, "s3://dl-processing-zone-341567884088/silver/titanic_delta")
 
 
 print("UPSERT...")
@@ -55,7 +55,7 @@ print("New data...")
 
 (
     spark.read.format("delta")
-    .load("s3://dl-processing-zone-539445819060/silver/titanic_delta")
+    .load("s3://dl-processing-zone-341567884088/silver/titanic_delta")
     .where("PassengerId < 6 OR PassengerId > 888")
     .show()
 )
@@ -64,7 +64,7 @@ print("Old data - with time travel")
 (
     spark.read.format("delta")
     .option("versionAsOf", "0")
-    .load("s3://dl-processing-zone-539445819060/silver/titanic_delta")
+    .load("s3://dl-processing-zone-341567884088/silver/titanic_delta")
     .where("PassengerId < 6 OR PassengerId > 888")
     .show()
 )
